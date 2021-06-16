dac0ef2c04843f74c06f9c57ddec69476e1cc416
unknown <aakashvats2910@gmail.com>
2021-06-17 01:18:30 +0530

    Updated visibility

--- main.py
+++ main.py
@@ -17,18 +17,27 @@ a = cv2.imread(file)
 
 # denoisfy the image
 # a = cv2.fastNlMeansDenoisingColored(a, None, 10, 10, 7, 21)
+# print("Deonisfying")
 
 # loop on each pixel value of image on all three channels.
+op = 1
+total = a.shape
+for i in total:
+    op *= i
+
+current = 0
 for i, i_ in enumerate(a):
     for j, j_ in enumerate(i_):
         for k, k_ in enumerate(j_):
+            current += 1
             tmp = a[i][j][k]
             if 2 <= tmp <= 5:
                 a[i][j][k] = 255-tmp
+            print("{} % complete".format(current/op*100))
 
 print("Save file in which directory?")
 save_dir = filedialog.askdirectory()
 img = Image.fromarray(a)
 
 # save in save_dir as img.jpeg
-img.save(save_dir + "/" + file_name + ".jpeg")
+img.save(save_dir + "/" + file_name + "_img.jpeg")
